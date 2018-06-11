630ededd1984399e4aa3f6e907efba240ba1176e
samdan <pri.samdan@gmail.com>
2018-06-11 22:44:04 +0800

    wtf is going on

--- bot.js
+++ bot.js
@@ -77,13 +77,10 @@ require("fs").readdirSync(normalizedPath).forEach(function(file) {
 console.log('broadcast');
     
 controller.api.broadcast.create_message_creative("Hello friend !", function (err, body) {
-    // Your awesome code here
-    console.log(body);
-    // And here
+    controller.api.broadcast.send(body['message_creative_id'], null, function (err, body) {
+        // Your awesome code here
+        console.log(body);
+        // And here
+    });
 });
 
-controller.api.broadcast.send(1, null, function (err, body) {
-    // Your awesome code here
-    console.log(body);
-    // And here
-});
