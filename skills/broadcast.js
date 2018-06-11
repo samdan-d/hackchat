5992fb1e8a15de342ac1886062d81fe196d5db13
samdan <pri.samdan@gmail.com>
2018-06-11 22:22:59 +0800

    wtf is going on

--- skills/broadcast.js
+++ skills/broadcast.js
@@ -4,13 +4,13 @@ module.exports = function(controller) {
     
     controller.api.broadcast.create_message_creative("Hello friend !", function (err, body) {
         // Your awesome code here
-        console.log(body['message_creative_id']);
+        console.log(body);
         // And here
     });
 
     controller.api.broadcast.send('1', null, function (err, body) {
         // Your awesome code here
-        console.log(body['broadcast_id']);
+        console.log(body);
         // And here
     });
 
